a367768bc9997eb10cd0554047df1bc2f1740f1b
collin-williford <collinswilliford@gmail.com>
2024-08-07 20:46:00 -0400

    basic api working

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,55 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type Account struct {
+	Type          string `json:"type"`
+	AccountNumber string `json:"accountnumber"`
+	BankName      string `json:"bankname"`
+	RoutingNumber int    `json:"routingnumber"`
+	Balance       int    `json:"balance"`
+}
+
+var accounts = []Account{
+	{Type: "Checking", AccountNumber: "9988445", BankName: "Wells Fargo", RoutingNumber: 72456, Balance: 24500},
+	{Type: "Savings", AccountNumber: "8877442", BankName: "Wells Fargo", RoutingNumber: 87432, Balance: 32500},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/accounts", getAccounts)
+	router.GET("/accounts/:accountNumber", getAccountByNumber)
+	router.POST("/accounts", postAccounts)
+
+	router.Run("localhost:8080")
+}
+
+func getAccounts(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, accounts)
+}
+
+func postAccounts(c *gin.Context) {
+	var newAccount Account
+
+	if err := c.BindJSON(&newAccount); err != nil {
+		return
+	}
+
+	accounts = append(accounts, newAccount)
+	c.IndentedJSON(http.StatusCreated, newAccount)
+}
+
+func getAccountByNumber(c *gin.Context) {
+	accountNumber := c.Param("accountNumber")
+
+	for _, a := range accounts {
+		if a.AccountNumber == accountNumber {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Account number not found"})
+}
